ba18b028093dc8e80293cba572ad4372bd8f575a
yura-webdev <retrogamespalu@gmail.com>
2024-12-19 13:22:47 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika Hamburger diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar sidebar untuk menghilangkan menu
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
